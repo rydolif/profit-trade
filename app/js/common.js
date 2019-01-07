82507313b18cbfa3f31da64ab9a31d38ff1e6749
rudolif <rudolifrudolif@gmail.com>
2019-01-08 01:41:11 +0200

    first commit

--- app/js/common.js
+++ app/js/common.js
@@ -157,3 +157,250 @@ $(function() {
   $(window).resize(function(event) {
     $('.chat__content').jScrollPane();
   });
+
+
+  // $( function() {
+  //   // назви відповідають логіці
+  //   // перше слово назва блоку
+  //   // date - кількість днів
+  //   // price - кількість грошей
+  //   // min- мінімальне значення
+  //   // max- максимальне значення
+
+  //   //------------------------------------------данні в переміних-------------------------
+
+  //     //--------------------------------------calculator-beginner-----------------------------
+  //       var beginner__date_min = 0;
+  //       $( "#beginner__date_min" ).text(beginner__date_min);
+
+  //       var beginner__date_max = 1;
+  //       $( "#beginner__date_max" ).text(beginner__date_max + " дней");
+
+  //       var beginner__price_min = 100;
+  //       $( "#beginner__price_min" ).text(beginner__price_min);
+
+  //       var beginner__price_max = 500;
+  //       $( "#beginner__price_max" ).text(beginner__price_max + " тенге");
+
+  //     //--------------------------------------calculator-basic-----------------------------
+  //       var basic__date_min = 1;
+  //       $( "#basic__date_min" ).text(basic__date_min);
+
+  //       var basic__date_max = 31;
+  //       $( "#basic__date_max" ).text(basic__date_max + " дней");
+
+  //       var basic__price_min = 1000;
+  //       $( "#basic__price_min" ).text(basic__price_min);
+
+  //       var basic__price_max = 5000;
+  //       $( "#basic__price_max" ).text(basic__price_max + " тенге");
+
+  //     //--------------------------------------calculator-experience-----------------------------
+  //       var experience__date_min = 1;
+  //       $( "#experience__date_min" ).text(experience__date_min);
+
+  //       var experience__date_max = 31;
+  //       $( "#experience__date_max" ).text(experience__date_max + " дней");
+
+  //       var experience__price_min = 10000;
+  //       $( "#experience__price_min" ).text(experience__price_min);
+
+  //       var experience__price_max = 25000;
+  //       $( "#experience__price_max" ).text(experience__price_max + " тенге");
+
+  //     //--------------------------------------calculator-partner-----------------------------
+  //       var partner__date_min = 1;
+  //       $( "#partner__date_min" ).text(partner__date_min);
+
+  //       var partner__date_max = 31;
+  //       $( "#partner__date_max" ).text(partner__date_max + " дней");
+
+  //       var partner__price_min = 50000;
+  //       $( "#partner__price_min" ).text(partner__price_min);
+
+  //       var partner__price_max = 250000;
+  //       $( "#partner__price_max" ).text(partner__price_max + " тенге");   
+
+  //     //--------------------------------------calculator-owner-----------------------------
+  //       var owner__date_min = 0;
+  //       $( "#owner__date_min" ).text(owner__date_min);
+
+  //       var owner__date_max = 1;
+  //       $( "#owner__date_max" ).text(owner__date_max + " дней");
+
+  //       var owner__price_min = 1000000;
+  //       $( "#owner__price_min" ).text(owner__price_min);
+
+  //       var owner__price_max = 10000000;
+  //       $( "#owner__price_max" ).text(owner__price_max + " тенге");
+
+
+  //   //------------------------------------------калькулятори беруть данні з переміних-------------------------
+
+  //     //--------------------------------------calculator-beginner-----------------------------
+  //       //   var beginner = $( "#beginner--date-custom" );
+
+  //       //   $( "#beginner--date" ).slider({
+  //         // range: "min",
+  //         // value: 0,
+  //         // min: beginner__date_min,
+  //         // max: beginner__date_max,
+  //       //     create: function() {
+  //       //       beginner.text( $( this ).slider( "value" ) );
+  //       //     },
+  //       //     slide: function( event, ui ) {
+  //       //       beginner.text( ui.value );
+  //       //       $( "#beginner--percent" ).val( ui.value * 100 + "%");
+  //       //     }
+  //       //   });
+  //       //   $( "#beginner--percent" ).val( 0 + "%"  );
+
+  //       //------------------------------------
+  //       var beginnerprice = $( "#beginner--price" );
+  //         $( "#beginner--custom-price" ).slider({
+  //         range: "min",
+  //         value: 0,
+  //         min: beginner__price_min,
+  //         max: beginner__price_max,
+  //         create: function() {
+  //               beginnerprice.text( $( this ).slider( "value" ) );
+  //           },
+  //           slide: function( event, ui ) {
+  //               beginnerprice.text( ui.value );
+  //             }
+  //         });
+
+
+  //     //--------------------------------------calculator-basic-----------------------------
+  //         var basic = $( "#basic--date-custom" );
+  //         $( "#basic--date" ).slider({
+  //         range: "min",
+  //         value: 0,
+  //         min: basic__date_min,
+  //         max: basic__date_max,
+  //           create: function() {
+  //             basic.text( $( this ).slider( "value" ) );
+  //           },
+  //           slide: function( event, ui ) {
+  //             basic.text( ui.value );
+  //             $( "#basic--percent" ).val( ui.value * 3 + 7 + "%");
+  //           }
+  //         });
+  //         $( "#basic--percent" ).val( 10 + "%"  );
+
+  //       //------------------------------------
+  //       var basicprice = $( "#basic--price" );
+  //         $( "#basic--custom-price" ).slider({
+  //         range: "min",
+  //         value: 0,
+  //         min: basic__price_min,
+  //         max: basic__price_max,
+  //         create: function() {
+  //               basicprice.text( $( this ).slider( "value" ) );
+  //           },
+  //           slide: function( event, ui ) {
+  //               basicprice.text( ui.value );
+  //             }
+  //         });
+
+
+  //     //--------------------------------------calculator-experience-----------------------------
+  //         var experience = $( "#experience--date-custom" );
+  //         $( "#experience--date" ).slider({
+  //         range: "min",
+  //         value: 1,
+  //         min: experience__date_min,
+  //         max: experience__date_max,
+  //           create: function() {
+  //             experience.text( $( this ).slider( "value" ) );
+  //           },
+  //           slide: function( event, ui ) {
+  //             experience.text( ui.value );
+  //             $( "#experience--percent" ).val( ui.value * 3 + 7 + "%");
+  //           }
+  //         });
+  //         $( "#experience--percent" ).val( 10 + "%" );
+
+  //       //------------------------------------
+  //       var experienceprice = $( "#experience--price" );
+  //         $( "#experience--custom-price" ).slider({
+  //         range: "min",
+  //         value: 1,
+  //         min: experience__price_min,
+  //         max: experience__price_max,
+  //         create: function() {
+  //               experienceprice.text( $( this ).slider( "value" ) );
+  //           },
+  //           slide: function( event, ui ) {
+  //               experienceprice.text( ui.value );
+  //             }
+  //         });
+
+
+  //     //--------------------------------------calculator-partner-----------------------------
+  //         var partner = $( "#partner--date-custom" );
+  //         $( "#partner--date" ).slider({
+  //         range: "min",
+  //         value: 0,
+  //         min: partner__date_min,
+  //         max: partner__date_max,
+  //           create: function() {
+  //             partner.text( $( this ).slider( "value" ) );
+  //           },
+  //           slide: function( event, ui ) {
+  //             partner.text( ui.value );
+  //             $( "#partner--percent" ).val( ui.value * 3 + 7 + "%" );
+  //           }
+  //         });
+  //         $( "#partner--percent" ).val( 10 + "%" );
+
+  //       //------------------------------------
+  //       var partnerprice = $( "#partner--price" );
+  //         $( "#partner--custom-price" ).slider({
+  //         range: "min",
+  //         value: 0,
+  //         min: partner__price_min,
+  //         max: partner__price_max,
+  //         create: function() {
+  //               partnerprice.text( $( this ).slider( "value" ) );
+  //           },
+  //           slide: function( event, ui ) {
+  //               partnerprice.text( ui.value );
+  //             }
+  //         });
+
+
+  //     //--------------------------------------calculator-owner-----------------------------
+  //         //     var owner = $( "#owner--date-custom" );
+  //         //   $( "#owner--date" ).slider({
+  //           // range: "min",
+  //           // value: 0,
+  //           // min: 0,
+  //           // max: 1,
+  //         //     create: function() {
+  //         //       owner.text( $( this ).slider( "value" ) );
+  //         //     },
+  //         //     slide: function( event, ui ) {
+  //         //       owner.text( ui.value );
+  //         //       $( "#owner--percent" ).val( ui.value * 100 + "%");
+  //         //     }
+  //         //   });
+  //         //   $( "#owner--percent" ).val( 0 + "%"  );
+
+  //       var ownerprice = $( "#owner--price" );
+  //         $( "#owner--custom-price" ).slider({
+  //         range: "min",
+  //         value: 0,
+  //         min: 1000000,
+  //         max: 10000000,
+  //         create: function() {
+  //               ownerprice.text( $( this ).slider( "value" ) );
+  //           },
+  //           slide: function( event, ui ) {
+  //               ownerprice.text( ui.value );
+  //             }
+  //         });
+
+  //   } );
+
+  // 

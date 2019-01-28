360feb2d670335376c74c2a7d26793267902dcf1
rudolif <rudolifrudolif@gmail.com>
2019-01-28 19:55:17 +0200

    правка

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,18 @@
 $(function() {
   
+// //---------------------------js-----------------------
+// $('.tabs__wrap').hide();
+// $('.tabs__wrap:first').show();
+// $('.tabs ul a:first').addClass('active');
+//  $('.tabs ul a').click(function(event){
+//   event.preventDefault();
+//   $('.tabs ul a').removeClass('active');
+//   $(this).addClass('active');
+//   $('.tabs__wrap').hide();
+//    var selectTab = $(this).attr('href');
+//   $(selectTab).fadeIn();
+// });
+
 //--------------------------------------scroll------------------------------
   $('.chat__content').jScrollPane();
 
